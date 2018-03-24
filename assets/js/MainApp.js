0a9c19a6a02452619f955e3880e418c79bd5610a
happysalada <raphael.megzari@gmail.com>
2018-03-24 20:42:24 +0900

    add a dummy form in additem page

--- assets/js/MainApp.js
+++ assets/js/MainApp.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Profile from './components/profile';
 import LandingSlide from './components/landingSlide';
+import AddItem from './components/additem';
 
 class MainApp extends Component {
   render() {
@@ -9,6 +10,7 @@ class MainApp extends Component {
       <React.Fragment>
         <Route path='/' exact component={LandingSlide} />
         <Route path='/profile' exact component={Profile} />
+        <Route path='/additem' exact component={AddItem} />
       </React.Fragment>
     );
   }
