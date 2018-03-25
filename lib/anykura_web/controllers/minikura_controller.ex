d1db0d9daf513ac41484e413d033b2ab4518126c
happysalada <raphael.megzari@gmail.com>
2018-03-25 11:05:52 +0900

    add update route

--- lib/anykura_web/controllers/minikura_controller.ex
+++ lib/anykura_web/controllers/minikura_controller.ex
@@ -3,8 +3,16 @@ defmodule AnykuraWeb.MinikuraController do
   require Logger
 
   def index(conn, params) do
-    IO.inspect(params)
     with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <- HTTPoison.get("https://junction-tokyo.minikura.com/v1/minikura/item?oem_key=a58f6f263c8b5e6b") do
+      conn
+      |> put_status(200)
+      |> json(Jason.decode!(body))
+    end
+  end
+
+  def update(conn, params) do
+    IO.inspect(params)
+    with {:ok, %HTTPoison.Response{status_code: 200, body: body}} <- HTTPoison.patch("https://junction-tokyo.minikura.com/v1/minikura/item?oem_key=a58f6f263c8b5e6b&item_id=#{params["item_id"]}&storage_status=#{params["storage_status"]}", %{}) do
       conn
       |> put_status(200)
       |> json(Jason.decode!(body))
