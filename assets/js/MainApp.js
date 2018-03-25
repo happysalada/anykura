3d6cc5dff8f4748e52aca361a2a8fab519c4576a
happysalada <raphael.megzari@gmail.com>
2018-03-25 11:18:12 +0900

    fix static assets

--- assets/js/MainApp.js
+++ assets/js/MainApp.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 import Profile from './components/profile';
-import LandingSlide from './components/landingSlide';
+import LandingSlide from './components/landing-slide';
 import StorageMap from './components/storagemap';
 import ItemStatus from './components/itemstatus';
 import AddItem from './components/additem';
@@ -10,11 +10,11 @@ class MainApp extends Component {
   render() {
     return (
       <React.Fragment>
-        <Route path='/' exact component={LandingSlide} />
-        <Route path='/profile' exact component={Profile} />
-        <Route path='/find-storage' exact component={StorageMap} />
-        <Route path='/view-item' exact component={ItemStatus} />
-        <Route path='/add-item' exact component={AddItem} />
+        <Route path="/" exact component={LandingSlide} />
+        <Route path="/profile" exact component={Profile} />
+        <Route path="/find-storage" exact component={StorageMap} />
+        <Route path="/view-item" exact component={ItemStatus} />
+        <Route path="/add-item" exact component={AddItem} />
       </React.Fragment>
     );
   }
